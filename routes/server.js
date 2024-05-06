c1f257d973f784d297fec920b9a59fa9eb506431
Nikhil Tandon <nikkidatikki@gmail.com>
2024-05-05 21:15:50 -0400

    servwe

--- routes/server.js
+++ routes/server.js
@@ -1,18 +1,8 @@
-require('dotenv').config(); // Load environment variables from .env file at the start
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const feedbackRoutes = require('./routes/feedbackRoutes'); // Ensure this path matches your file structure
-const cors = require('cors'); // Typically, you would use the cors package
+const feedbackRoutes = require('./routes/feedbackRoutes'); // Make sure the path matches your folder structure
 
 const app = express();
-
-// Setup CORS (Cross-Origin Resource Sharing) options if needed
-app.use(cors({
-    origin: '*', // Adjust according to your needs, '*' for open access
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
-    credentials: true // Enable credentials if your front end needs to pass auth tokens
-}));
 
 // Middleware to parse JSON and URL-encoded form data
 app.use(bodyParser.json());
