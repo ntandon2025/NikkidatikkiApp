de86d152c15e742fcb9364f59522f2edf00ffba3
Nikhil Tandon <nikkidatikki@gmail.com>
2024-05-11 17:59:55 -0400

    fireworks

--- game23.js
+++ game23.js
@@ -155,13 +155,36 @@ function goToGame23Page() {
     victorySound.play();
 }
 function startConfetti() {
-    console.log('Confetti button clicked');
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 }
-    });
+    var duration = 15 * 1000; // 15 seconds in milliseconds
+    var animationEnd = Date.now() + duration;
+    var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+    function randomInRange(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    var interval = setInterval(function() {
+        var timeLeft = animationEnd - Date.now();
+
+        if (timeLeft <= 0) {
+            return clearInterval(interval);
+        }
+
+        var particleCount = 50 * (timeLeft / duration);
+        // Create confetti from different origins on the screen
+        confetti({
+            ...defaults,
+            particleCount,
+            origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+        });
+        confetti({
+            ...defaults,
+            particleCount,
+            origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+        });
+    }, 250);
 }
+
 
 document.addEventListener('keydown', (event) => {
     console.log(event.key);  // Check which key is pressed
